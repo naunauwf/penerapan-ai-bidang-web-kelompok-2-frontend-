4f489f4fe6589134a5dc2902878daffa0eb0065d
nau <wahyuyahaha111@gmail.com>
2026-03-11 13:48:56 +0700

    Revert "change logic base"
    This reverts commit 7ceacb865b8b33d39997737fb3f5c6765e4862cd.

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base:
-    command === "build"
-      ? "/penerapan-ai-bidang-web-kelompok-2-frontend-/"
-      : "/",
-}));  
+  base: "/penerapan-ai-bidang-web-kelompok-2-frontend-/",
+});
