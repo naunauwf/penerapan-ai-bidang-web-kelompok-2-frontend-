7ceacb865b8b33d39997737fb3f5c6765e4862cd
nau <wahyuyahaha111@gmail.com>
2026-03-11 13:44:25 +0700

    change logic base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/penerapan-ai-bidang-web-kelompok-2-frontend-/",
-});
+  base:
+    command === "build"
+      ? "/penerapan-ai-bidang-web-kelompok-2-frontend-/"
+      : "/",
+}));  
